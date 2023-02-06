45662a4e8a64f38ae9c8aba5bf50f96eed94f207
jamiefrazier <122464823+jamiefrazier@users.noreply.github.com>
2023-02-05 17:42:55 -0700

    homePage wireFrame

--- client/src/views/homePage/index.jsx
+++ client/src/views/homePage/index.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+
+    <Box fluid >
+        <Typography
+        variant="h1"
+        fontWeight="500"
+        sx={{ color: colors.grey[500] }}
+      >
+        <div id="welcome-banner-section"
+        style={{ height: '20vh',background: colors.primary[700] }}>welcome banner section</div>
+
+      </Typography>
+      <Typography
+        variant="h1"
+        fontWeight="500"
+        height="500"
+        sx={{ color: colors.grey[500] }}
+      >
+        <div id="line-graph-section"
+        style={{ height: '30vh',background: colors.primary[500] }}>Line graph section</div>
+
+      </Typography>
+      <Typography
+        variant="h1"
+        fontWeight="500"
+        sx={{ color: colors.grey[500] }}
+      >
+        <div id="bar-graph-section"
+        style={{ height: '40vh' , background: colors.primary[700]}}>
+          bar graph section</div>
+
+      </Typography>
+
+    </Box>
+   )
+
 };
 
 export default HomePage;
+// I made different background shades so you could see the
+// individual sections

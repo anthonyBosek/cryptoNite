ef727ccb63fe2a1b83b0c86f825048a451ee894a
Anthony Bosek <anthony.bosek@gmail.com>
2023-02-06 19:07:05 -0700

    CRYP-10 final api checks

--- config/db.config.js
+++ config/db.config.js
@@ -3,41 +3,26 @@ const { MongoMemoryServer } = require("mongodb-memory-server");
 
 const config = require("./index");
 
-let mongoDB = null; // * placeholder for our mock DB instance
+let mongoDB = null;
 
 const connectDB = async () => {
   try {
-    let dbUrl = config.MONGO_URI; //* store our uri to var that we'll use to connect with
+    let dbUrl = config.MONGO_URI;
 
-    //* if our runtime env is testing then we'll change our connection strategy
     if (config.NODE_ENV === "test") {
-      //* create a temp mock mongo server in runtime
       mongoDB = await MongoMemoryServer.create();
-
-      //* fetch mock mongo uri and reassign to
       dbUrl = mongoDB.getUri();
     }
-    mongoose.set("strictQuery", true); //* upcoming deprecation warning in future release, set strictQuery to true before connecting
+    mongoose.set("strictQuery", true);
 
-    /**
-     * @function import('mongoose').connect - connection promise
-     * @param {string} uri - uri to connect to - required
-     * @param {object} [options] - connection options - optional
-     * @param {Function} [callback] - callback function - optional
-     * @returns {Promise<void>}
-     */
-    console.log(dbUrl)
-    console.log(config.NODE_ENV)
     const conn = await mongoose.connect(dbUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    //* log connection info if not in test env
     if (config.NODE_ENV !== "test")
       console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
-    //* log error if not in test env else throw error
     if (config.NODE_ENV !== "test") {
       console.error(err);
       process.exit(1);

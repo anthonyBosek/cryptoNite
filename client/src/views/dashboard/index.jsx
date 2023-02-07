b32fea81e414fe939ce49af49dfbce3bb30e1747
Anthony Bosek <anthony.bosek@gmail.com>
2023-02-07 15:23:10 -0700

    API coin array, API one coin data

--- client/src/views/dashboard/index.jsx
+++ client/src/views/dashboard/index.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
 import Header from "components/Header";
+import LineGraph from "components/LineGraph";
 
 const Dashboard = () => {
   return (
     <Box m="20px">
       <Header title="Dashboard" subtitle="Your CryptoNite Accounts" />
+      <LineGraph />
     </Box>
   );
 };
